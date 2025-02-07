aa6867b5dd966696b80b1ac5b8a6dc0127414a2e
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-02-07 16:23:34 -0500

    Add random number

--- Lab1_Huanbing_Zhang/ContentView.swift
+++ Lab1_Huanbing_Zhang/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number: Int = Int.random(in: 1...100)
     var body: some View {
         VStack {
             Image(systemName: "globe")
